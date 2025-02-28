a94f4e03021e37b971698865f3ecd6c86654e7c3
Guus Kolpa <guus.kolpa@gmail.com>
2025-02-28 14:47:19 +0100

    Initil commit
    Reconfigured

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    testMatch: ["**/tests/**/*.test.ts"],  // Look for test files under 'tests'
+    transform: {
+      "^.+\\.ts$": "ts-jest", // Handle TypeScript with ts-jest
+    },
+  };
